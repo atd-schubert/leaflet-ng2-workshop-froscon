a50f8ed2c6e56352fde25b90467468cec6077516
Arne Schubert <atd.schubert@gmail.com>
2017-08-18 23:09:43 +0200

    Add the OSM tile URL to the controller

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public tileUrl: string = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
   constructor(public navCtrl: NavController) {
 
